dfab32da7fc05bc50d94488a1166096658e4b13b
Zak Stern <zak.stern@gmail.com>
2013-08-10 14:11:34 -0700

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RerideApp::Application.config.secret_key_base = '65ba33bfcff1cd150c0d8f8783b83d71e22d11685576fb7844e5a037ebf129ceace8ed9273d274e9553de7c54f25b787c6f832611c4fa1383c4b2e2eb22f0701'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RerideApp::Application.config.session_store :cookie_store, key: '_reride_app_session'
