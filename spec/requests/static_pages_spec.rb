135e891d354af9f130d044df472609863d3f3261
Zak Stern <zak.stern@gmail.com>
2013-08-10 16:36:25 -0700

    add static page tests and refactor layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,46 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
+describe "Static Pages" do
+
+	subject { page }
+
+  	shared_examples_for "all static pages" do
+    	it { should have_selector('h1', text: heading) }
+    	it { should have_title(full_title(page_title)) }
+  	end
+
+ 	describe "Home Page" do
+ 		let(:heading)    { 'ReRide Bikes' }
+    	let(:page_title) { '' }
+ 		before { visit root_path }
+
+ 		it_should_behave_like "all static pages"
+	end 
+
+	describe "Help Page" do
+
+		let(:heading)    { 'Help' }
+    	let(:page_title) { 'Help' }
+ 		before { visit help_path }
+
+ 		it_should_behave_like "all static pages"
+	end 
+
+	describe "About Page" do
+
+		let(:heading)    { 'About Us' }
+    	let(:page_title) { 'About' }
+ 		before { visit about_path }
+
+ 		it_should_behave_like "all static pages"
+	end
+
+	describe "Contact Page" do
+
+		let(:heading)    { 'Contact' }
+    	let(:page_title) { 'Contact' }
+ 		before { visit contact_path }
+
+ 		it_should_behave_like "all static pages"
+	end
 end
