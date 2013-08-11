17af4a3a7ce838d49e6d4dd4b1e0c5ccae6ad266
Zak Stern <zak.stern@gmail.com>
2013-08-10 18:41:57 -0700

    add styling and layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,16 @@ describe "Static Pages" do
 
  		it_should_behave_like "all static pages"
 	end
+	it "should have the right links on the layout" do
+    	visit root_path
+    	click_link "About"
+    	expect(page).to have_title(full_title('About'))
+    	click_link "Help"
+    	expect(page).to have_title(full_title('Help'))
+    	click_link "Contact"
+    	expect(page).to have_title(full_title('Contact'))
+    	click_link "Home"
+        first(:link, "ReRide").click
+    	expect(page).to have_title(full_title(''))
+  end
 end
