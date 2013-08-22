f09ed87992d6fa4ac4b4fb51fbecf29c462acfd4
Zak Stern <zak.stern@gmail.com>
2013-08-21 23:01:52 -0700

    fix tests

--- spec/requests/bike_shop_pages_spec.rb
+++ spec/requests/bike_shop_pages_spec.rb
@@ -24,21 +24,8 @@ describe "BikeShopPages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Shop Name",         with: "Example Store"
-        fill_in "Street Address",	with: "100 Fake Street"
-        fill_in "City",        	with: "City"
-        fill_in "State",     	with: "CA"
-        fill_in "Zip Code", 	with: "90405"
-        fill_in "Phone Number",	with: "999-999-9999"
-        fill_in "Website",		with: "http://www.example.com"
-        fill_in "First Name",	with: "Jim"
-        fill_in "Last Name",	with: "Smith"
-        fill_in "Email", 		with: "fake@fake.com"
-     	fill_in "Password",		with: "temporary", :match => :prefer_exact
-     	fill_in "Password Confirmation:", with: "temporary", :match => :prefer_exact
-      end
-
+      before { valid_bike_shop_signup }
+      
       it "should create a bike shop" do
         expect { click_button submit }.to change(BikeShop, :count).by(1)
       end
@@ -53,7 +40,7 @@ describe "BikeShopPages" do
 	    let(:bike_shop) { FactoryGirl.create(:bike_shop) }
 	    
 	    before do 
-	    	bike_shop.users.build(first_name: "First", last_name: "Last", email: "example@example.com", 
+	    	bike_shop.users.create(first_name: "First", last_name: "Last", email: "example@example.com", 
     		password: "foobar", password_confirmation: "foobar") 
 	    	visit bike_shop_path(bike_shop)
 	    end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,23 @@
 include ApplicationHelper
+
+def valid_user_signup
+	fill_in "Name",         with: "Example User"
+  	fill_in "Email",        with: "user@example.com"
+  	fill_in "Password",     with: "foobar"
+  	fill_in "Confirm Password", with: "foobar"
+end
+
+def valid_bike_shop_signup
+	fill_in "Shop Name",         with: "Example Store"
+    fill_in "Street Address",	with: "100 Fake Street"
+    fill_in "City",        	with: "City"
+    fill_in "State",     	with: "CA"
+    fill_in "Zip Code", 	with: "90405"
+    fill_in "Phone Number",	with: "999-999-9999"
+    fill_in "Website",		with: "http://www.example.com"
+    fill_in "First Name",	with: "Jim"
+    fill_in "Last Name",	with: "Smith"
+    fill_in "Email", 		with: "fake@fake.com"
+ 	fill_in "Password",		with: "temporary", :match => :prefer_exact
+ 	fill_in "Password Confirmation:", with: "temporary", :match => :prefer_exact
+end
